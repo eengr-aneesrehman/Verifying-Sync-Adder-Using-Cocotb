23756e3f11ca6233b3a00c6bbfe5d935f5563d90
anees_rehman <t_arehman@xcelerium.com>
2024-05-26 07:44:44 +0000

    clean up

--- verification/test_adder/sim_build/verilated.d
+++
@@ -1,9 +0,0 @@
-verilated.o: \
- /tools/verilator/v4.034/share/verilator/include/verilated.cpp \
- /tools/verilator/v4.034/share/verilator/include/verilatedos.h \
- /tools/verilator/v4.034/share/verilator/include/verilated_imp.h \
- /tools/verilator/v4.034/share/verilator/include/verilated.h \
- /tools/verilator/v4.034/share/verilator/include/verilated_heavy.h \
- /tools/verilator/v4.034/share/verilator/include/verilated_syms.h \
- /tools/verilator/v4.034/share/verilator/include/verilated_sym_props.h \
- /tools/verilator/v4.034/share/verilator/include/verilated_config.h
